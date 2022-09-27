bae85d2f98f8151d004270feca88c176d1ac1b46
DanielJoung <djoung100@hotmail.com>
2022-09-26 20:18:45 -0400

    add edit on post

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,11 +3,28 @@ const router = express.Router();
 
 const ctrls = require("../controllers");
 
+const authRequired = (req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect(``);
+  }
+};
+
+const authRequiredCreate = (req, res, next) => {
+  if (req.session.currentUser) {
+    next();
+  } else {
+    res.redirect(``);
+  }
+};
+
 // router
 router.get("/", (req, res) => {});
 router.get("/post", ctrls.posts.index);
-router.post("/post", ctrls.posts.create);
+router.post("/post", authRequiredCreate, ctrls.posts.create);
 router.delete("/post/:id", ctrls.posts.destroy);
+router.get("/post/:id/edit", authRequired, ctrls.posts.edit);
 router.put("/post/:id", ctrls.posts.update);
 
 module.exports = router;
