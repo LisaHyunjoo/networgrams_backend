3a5ae8a31e245645dff7bbf6aa6f4ce8a2974364
Lisa <lisahongofficial@gmail.com>
2022-09-27 22:33:13 -0400

    conflicts are updated

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,13 +3,6 @@ const router = express.Router();
 
 const ctrls = require("../controllers");
 
-<<<<<<< HEAD
-// router
-router.get("/", ctrls.posts.index);
-router.post("/", ctrls.posts.create);
-router.delete("/:id", ctrls.posts.destroy);
-router.put("/:id", ctrls.posts.update);
-=======
 const authRequired = (req, res, next) => {
   if (req.session.currentUser) {
     next();
@@ -35,6 +28,5 @@ router.post("/post", ctrls.posts.create);
 router.delete("/post/:id", ctrls.posts.destroy);
 router.get("/post/:id/edit", authRequired, ctrls.posts.edit);
 router.put("/post/:id", ctrls.posts.update);
->>>>>>> 10f36d2b770740b2c609775aeab9dd31e3eeac55
 
 module.exports = router;
