109f31f410b062c673206a210214eab017d0cb96
DanielJoung <djoung100@hotmail.com>
2022-10-01 13:02:37 -0400

    fix comment routes

--- controllers/commentCtrils.js
+++ controllers/commentCtrils.js
@@ -1,7 +1,7 @@
 const db = require("../models");
 
 const index = (req, res) => {
-  db.Comments.find({}, (err, comment) => {
+  db.Comments.find(req.params.id, (err, comment) => {
     if (err) return res.status(404).json({ error: err.message });
     return res.status(200).json({
       comment,
@@ -24,6 +24,7 @@ const destroy = (req, res) => {
     if (err) return res.status(404).json({ error: err.message });
     return res.status(200).json({
       message: "deleted",
+      deletedComment,
     });
   });
 };
