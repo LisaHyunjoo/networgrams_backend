ea4a10342bcbe239e6678d9f157cfd317350a12f
DanielJoung <djoung100@hotmail.com>
2022-10-01 15:08:00 -0400

    post show route

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -3,14 +3,6 @@ const router = express.Router();
 
 const ctrls = require("../controllers");
 
-const authRequired = (req, res, next) => {
-  if (req.session.currentUser) {
-    next();
-  } else {
-    res.redirect(``);
-  }
-};
-
 // router
 router.get("/", (req, res) => {
   res.json();
@@ -18,7 +10,8 @@ router.get("/", (req, res) => {
 router.get("/post", ctrls.posts.index);
 router.post("/post", ctrls.posts.create);
 router.delete("/post/:id", ctrls.posts.destroy);
-router.get("/post/:id/edit", authRequired, ctrls.posts.edit);
+router.get("/post/:id", ctrls.posts.show);
+router.get("/post/:id/edit", ctrls.posts.edit);
 router.put("/post/:id", ctrls.posts.update);
 
 module.exports = router;
